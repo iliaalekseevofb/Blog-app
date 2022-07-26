4672a4ed2f3efc3f20e1587a61114a6742376b24
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-26 15:54:01 +0500

    Firebase is set up, login with Google is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
+import Login from './pages/Login';
 import './App.css'
 
 const App = () => {
+
+  const [isAuth, setIsAuth] = useState(false)
+
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/createpost'>Create post</Link>
+        {!isAuth && <Link to='/login'>Login</Link>}
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/createpost' element={<CreatePost />} />
+        <Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/firebase-config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBhYtnGI4svEIrMmv2AZP9M_BRaCu_rQw",
+  authDomain: "blog-app-31c11.firebaseapp.com",
+  projectId: "blog-app-31c11",
+  storageBucket: "blog-app-31c11.appspot.com",
+  messagingSenderId: "445878318051",
+  appId: "1:445878318051:web:5af17e2b592529445ee6d6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
