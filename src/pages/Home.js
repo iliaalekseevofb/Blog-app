29160027776f3a7706748cffe3a20610803d3751
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-27 02:56:56 +0500

    Structurized post's data is being displayed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import {getDocs, collection, doc} from 'firebase/firestore';
+import { db } from '../firebase-config';
 
 const Home = () => {
+
+  const [postLists, setPostLists] = useState([]);
+  const postsCollectionRef = collection(db, 'posts');
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostLists(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+    getPosts();
+  });
+
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      {postLists.map((post) => {
+        return (
+          <div className='post'>
+            <div className='postHeader'>
+              <div className='title'>
+                <h1>{post.title}</h1>
+              </div>
+            </div>
+            <div className='postTextContainer'>
+              {post.postText}
+            </div>
+            <h3>@ {post.author.name}</h3>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
